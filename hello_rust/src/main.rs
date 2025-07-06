69767cb10b549bae6d47f3a9aa5b05e23511da4c
alexander-py <alexanderphoenixbusiness@mail.bg>
2025-07-06 18:24:19 +0100

    feat(learning): control flow

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,7 +1,18 @@
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+// fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+//
+// fn main() {
+//     println!("1330 + 7 = {}", add(1330, 7));
+// }
+
 
 fn main() {
-    println!("1330 + 7 = {}", add(1330, 7));
+    for i in 0..11 {
+        if i % 2 == 0 {
+            println!("is even ---> {}", i)
+        } else {
+            println!("is not even ---> {}", i)
+        }
+    }
 }
