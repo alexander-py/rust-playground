920f77261dfb8145727010c79a05a0385b0f1a74
alexander-py <alexanderphoenixbusiness@mail.bg>
2025-07-06 18:18:02 +0100

    feat(learning): vars and types

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    println!("Hello!");
-    println!("I am Alexander");
+
+    let x: i32 = 500;
+
+    // assume i32
+    let mut y = 5; // declaring a mutable
+    y +=1;
+
+    println!("x= {}, y={}", x, y);
+
+
 }
